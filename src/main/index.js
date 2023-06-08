c6087e60bb47b9d63d4d3c15b6e5551bc38dd4b2
taewoo <rlaxodn322@gmail.com>
2023-06-08 11:45:20 +0900

    Link 태그 적용

--- src/main/index.js
+++ src/main/index.js
@@ -1,22 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
-  React.useEffect(function(){
+  React.useEffect(function () {
     axios
-    .get("https://c6ed8d23-1c8f-43fa-888e-2c716f64e44e.mock.pstmn.io/products")
-    .then(function (result) {
-      console.log(result);
-      const products = result.data.product;
-      setProducts(products);
-    })
-    .catch(function (error) {
-      console.error("에러 발생:", error);
-    });
-  },[])
-  
+      .get(
+        "https://c6ed8d23-1c8f-43fa-888e-2c716f64e44e.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        console.log(result);
+        const products = result.data.product;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생:", error);
+      });
+  }, []);
+
   return (
     <div>
       <div id="header">
@@ -33,25 +36,27 @@ function MainPage() {
           {products.map(function (product, index) {
             return (
               <div className="product-card">
-                <div>
-                  <img
-                    className="product-img"
-                    src={product.imageUrl}
-                    alt="이미지"
-                  />
-                </div>
-                <div className="product-contents">
-                  <span className="product-name">{product.name}</span>
-                  <span className="product-price">{product.price}원</span>
-                  <div className="product-seller">
+                <Link className="product-link" to={`/products/${index}`}>
+                  <div>
                     <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
-                      alt="아바타"
+                      className="product-img"
+                      src={product.imageUrl}
+                      alt="이미지"
                     />
-                    <span>{product.seller}</span>
                   </div>
-                </div>
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                        alt="아바타"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </Link>
               </div>
             );
           })}
