8c35e2549ac5d9b4f8f567c366372f876d568715
taewoo <rlaxodn322@gmail.com>
2023-06-08 10:44:44 +0900

    project init

---
+++ src/main/index.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import "./index.css";
+import axios from "axios";
+
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function(){
+    axios
+    .get("https://c6ed8d23-1c8f-43fa-888e-2c716f64e44e.mock.pstmn.io/products")
+    .then(function (result) {
+      console.log(result);
+      const products = result.data.product;
+      setProducts(products);
+    })
+    .catch(function (error) {
+      console.error("에러 발생:", error);
+    });
+  },[])
+  
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="./images/icons/logo.png" alt="로고" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="./images/banners/banner1.PNG" alt="배너" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img
+                    className="product-img"
+                    src={product.imageUrl}
+                    alt="이미지"
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                      alt="아바타"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
+
+export default MainPage;
