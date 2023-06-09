c5af96a42eb33881db2d04664bdf8d496b4408d7
taewoo <rlaxodn322@gmail.com>
2023-06-09 10:55:52 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
+import "antd/dist/antd.min.css";
 import "./App.css";
 import MainPage from "./main/index.js";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 function App() {
+  const history = useHistory();
   return (
     <div>
-      <Switch>
-        <Route exact={true} path={"/"}>
-          <MainPage />
-        </Route>
-        <Route exact={true} path={"/products/:id"}>
-          <ProductPage/>
-        </Route>
-        <Route exact={true} path={"/upload"}>
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" alt="로고" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push('/upload');
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={"/"}>
+            <MainPage />
+          </Route>
+          <Route exact={true} path={"/products/:id"}>
+            <ProductPage />
+          </Route>
+          <Route exact={true} path={"/upload"}>
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/main/index.js
+++ src/main/index.js
@@ -22,47 +22,39 @@ function MainPage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="./images/icons/logo.png" alt="로고" />
-        </div>
+      <div id="banner">
+        <img src="./images/banners/banner1.PNG" alt="배너" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="./images/banners/banner1.PNG" alt="배너" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={`/products/${index}`}>
-                  <div>
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img
+                    className="product-img"
+                    src={product.imageUrl}
+                    alt="이미지"
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
                     <img
-                      className="product-img"
-                      src={product.imageUrl}
-                      alt="이미지"
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                      alt="아바타"
                     />
+                    <span>{product.seller}</span>
                   </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <div className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                        alt="아바타"
-                      />
-                      <span>{product.seller}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
